de2b2907bae5b9aa85cae5d96fb1df4f128d4f87
Yuhan Xi <ac19971212@126.com>
2018-06-29 23:31:18 +0800

    -

---
+++ 基础编程题目集/函数题/6-12 判断奇偶性（10 分）.cpp
@@ -0,0 +1,52 @@
+﻿6 - 12 判断奇偶性（10 分）
+
+本题要求实现判断给定整数奇偶性的函数。
+
+函数接口定义：
+
+int even(int n);
+其中n是用户传入的整型参数。当n为偶数时，函数返回1；n为奇数时返回0。注意：0是偶数。
+
+裁判测试程序样例：
+
+#include <stdio.h>
+
+int even(int n);
+
+int main()
+{
+	int n;
+
+	scanf("%d", &n);
+	if (even(n))
+		printf("%d is even.\n", n);
+	else
+		printf("%d is odd.\n", n);
+
+	return 0;
+}
+
+/* 你的代码将被嵌在这里 */
+输入样例1：
+
+- 6
+输出样例1：
+
+- 6 is even.
+输入样例2：
+
+5
+输出样例2：
+
+5 is odd.
+
+送分。
+
+int even(int n) {
+	if (n % 2 == 0) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
